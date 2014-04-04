1af566e81bb262bf5caf87a45d24f8100950f21d
qucc <757518614@qq.com>
2014-04-04 10:58:30 +0800

    commiet

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+
+            ImageDownloader imageDownloader = new ImageDownloader(textBox1.Text);
+            imageDownloader.LoadCompleted += imageDownloader_LoadCompleted;
+            imageDownloader.LoadProgressChanged += imageDownloader_LoadProgressChanged;
+            imageDownloader.DownloadAsync();
+        }
+
+        private void imageDownloader_LoadProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            label1.Text= e.ProgressPercentage.ToString();
+        }
+
+        private void imageDownloader_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                Image img = (Image)e.UserState;
+                pictureBox1.Image = img;
+            }
+            else
+            {
+                Debug.Fail(e.Error.Message);
+            }
+        }
+    }
+}
---
+++ ImageDownloader.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageDownloader
+{
+    public  class ImageDownloader 
+    {
+        string url;
+        private Image errorImage;
+        private long contentLength;
+        private long totalBytesRead;
+        private MemoryStream tempDownloadStream;
+        private const int readBlockSize = 2096;
+        private byte[] readBuffer = null;
+        private AsyncOperation currentAsycLoadOperation = null;
+        private SendOrPostCallback loadCompletedDelegate = null;
+        private SendOrPostCallback loadProgressDelegate = null;
+       
+
+        public ImageDownloader(string url)
+        {
+            this.url = url;
+        }
+
+        public Image ErrorImage
+        {
+            get
+            {
+                return new Bitmap(typeof(PictureBox),"ImageInError.bmp");
+            }
+        }
+
+        public void DownloadAsync()
+        {
+            
+            currentAsycLoadOperation = AsyncOperationManager.CreateOperation(null);
+            if (loadCompletedDelegate == null)
+            {
+                loadCompletedDelegate = new SendOrPostCallback(LoadCompletedDelegate);
+                loadProgressDelegate = new SendOrPostCallback(LoadProgressDelegate);
+                readBuffer = new byte[readBlockSize];
+            }
+            tempDownloadStream = new MemoryStream();
+
+            WebRequest req = WebRequest.Create(url);
+            (new WaitCallback(BeginGetResponseDelegate)).BeginInvoke(req, null, null);
+ 
+        }
+
+
+        private void BeginGetResponseDelegate(object state)
+        {
+            WebRequest req = (WebRequest)state;
+            req.BeginGetResponse(new AsyncCallback(GetResponseCallback), req);
+        }
+
+        private void PostCompleted(Exception error, bool cancelled)
+        {
+            AsyncOperation temp = currentAsycLoadOperation;
+            currentAsycLoadOperation = null;
+            if (temp != null)
+            {
+                temp.PostOperationCompleted(loadCompletedDelegate, new AsyncCompletedEventArgs(error,cancelled,null));
+            }
+        }
+
+        private void LoadProgressDelegate(object state)
+        {
+            OnLoadProgressChanged((ProgressChangedEventArgs)state);
+        }
+
+        private void LoadCompletedDelegate(object state)
+        {
+            AsyncCompletedEventArgs e = (AsyncCompletedEventArgs)state;
+            Image img = errorImage;
+            if (!e.Cancelled && e.Error == null)
+            {
+                //successful completion
+                try
+                {
+                    img = Image.FromStream(tempDownloadStream);
+                    e = new AsyncCompletedEventArgs(e.Error, e.Cancelled, img);
+                }
+                catch (Exception error)
+                {
+                    e = new AsyncCompletedEventArgs(error,false, null);
+                }
+            }
+
+            tempDownloadStream = null;
+            OnLoadCompleted(e);
+        }
+
+        private void GetResponseCallback(IAsyncResult ar)
+        {
+            try
+            {
+                WebRequest req = (WebRequest)ar.AsyncState;
+                WebResponse webResponse = req.EndGetResponse(ar);
+                contentLength = webResponse.ContentLength;
+                totalBytesRead = 0;
+                Stream responseStream = webResponse.GetResponseStream();
+                responseStream.BeginRead(
+                    readBuffer,
+                    0,
+                    readBlockSize,
+                    new AsyncCallback(ReadCallback),
+                    responseStream);
+
+            }
+            catch (Exception error)
+            {
+                PostCompleted(error, false);
+            }
+        }
+
+
+
+        private void ReadCallback(IAsyncResult ar)
+        {
+            Stream responseStream = (Stream)ar.AsyncState;
+            try
+            {
+                int bytesRead = responseStream.EndRead(ar);
+                if (bytesRead > 0)
+                {
+                    totalBytesRead += bytesRead;
+                    tempDownloadStream.Write(readBuffer, 0, bytesRead);
+                    responseStream.BeginRead(
+                        readBuffer,
+                        0,
+                        readBlockSize,
+                        new AsyncCallback(ReadCallback),
+                        responseStream);
+                    if (contentLength != -1)
+                    {
+                        int progress = (int)(100 * (((float)totalBytesRead) / ((float)contentLength)));
+                        if (currentAsycLoadOperation != null)
+                        {
+                            currentAsycLoadOperation.Post(loadProgressDelegate, new ProgressChangedEventArgs(progress, null));
+                        }
+                    }
+                }
+                else
+                {
+                    tempDownloadStream.Seek(0, SeekOrigin.Begin);
+                    if (currentAsycLoadOperation != null)
+                    {
+                        currentAsycLoadOperation.Post(loadProgressDelegate, new ProgressChangedEventArgs(100, null));
+                    }
+                    PostCompleted(null,false);
+
+                    Stream rs = responseStream;
+                    responseStream = null;
+                    responseStream.Close();
+                }
+            }
+            catch (Exception error)
+            {
+                PostCompleted(error, false);
+                if (responseStream != null)
+                {
+                    responseStream.Close();
+                }
+            }
+        }
+
+        protected virtual void OnLoadProgressChanged(ProgressChangedEventArgs e)
+        {
+            ProgressChangedEventHandler handler = LoadProgressChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected virtual void OnLoadCompleted(AsyncCompletedEventArgs e)
+        {
+            if (LoadCompleted != null)
+            {
+                LoadCompleted(this, e);
+            }
+        }
+
+        public event AsyncCompletedEventHandler LoadCompleted;
+
+        public event ProgressChangedEventHandler LoadProgressChanged;
+    }
+}
